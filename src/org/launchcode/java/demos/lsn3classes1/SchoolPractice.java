f4134bfd5c2ad53885a361a6cff507254675c572
mpbelz <mpbelz@gmail.com>
2022-01-14 12:22:21 -0600

    Java Lesson 3 Exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,12 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student matt = new Student();
+        matt.setName("Matt");
+        matt.setNumberOfCredits(1);
+        matt.setGpa(4.0);
+        System.out.println("Student's Name: " + matt.getName());
+        System.out.println("Student's GPA: " + matt.getGpa());
+        System.out.println("Student's Credits: " + matt.getNumberOfCredits());
     }
 }
