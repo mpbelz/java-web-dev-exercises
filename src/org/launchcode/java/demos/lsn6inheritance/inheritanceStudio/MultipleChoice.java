90004da9cc89671f953a0505225bddd4e6999cbc
mpbelz <mpbelz@gmail.com>
2022-01-28 15:31:22 -0600

    Inheritance Studio

---
+++ src/org/launchcode/java/demos/lsn6inheritance/inheritanceStudio/MultipleChoice.java
@@ -0,0 +1,23 @@
+package org.launchcode.java.demos.lsn6inheritance.inheritanceStudio;
+
+public class MultipleChoice extends Question {
+    //properties
+
+
+    //constructor
+    public MultipleChoice(String question, String answer) {
+        super(question, answer);
+    }
+    //methods
+//    @Override
+//    public boolean checkAnswer(String answer) {
+//        //answer is "A" or "B"
+//
+//        String actualAnswer = this.getTheAnswer();
+//        if (answer.toUpperCase().equals(actualAnswer.toUpperCase())) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+}
