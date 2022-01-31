e7f8face4493453f8b2b915962b352b534ab7330
mpbelz <mpbelz@gmail.com>
2022-01-31 16:38:03 -0600

    Lesson 7 Interfaces Exercises

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -3,17 +3,31 @@ package org.launchcode.java.demos.lsn7interfaces;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        flavors.sort(new FlavorComparator());
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        cones.sort(new ConeComparator());
+
+        toppings.sort(new ToppingComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName() + " contains " + flavor.getAllergens().size() + " allergens");
+        }
+        for (Cone cone : cones) {
+            System.out.println(cone.getName() + ": $" + cone.getCost());
+        }
+        for (Topping topping : toppings) {
+            System.out.println(topping.getName() + ": $" + topping.getCost());
+        }
     }
 }
